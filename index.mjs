ca45463d94cf7b6e9a974e3b4d839f6bae0c3fe0
saintabomination <saint.abomination@protonmail.com>
2022-04-02 23:21:04 +0200

    Adding and setting up socket.io

--- index.mjs
+++ index.mjs
@@ -1,8 +1,19 @@
 import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 3001;
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-app.listen(port, () => {
+app.use(cors());
+
+io.on('connection', socket => {
+  console.log(`${socket}: Join`);
+});
+
+httpServer.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
