6edcc7f6dce0eaf01d0fc93ff98c798230160903
saintabomination <saint.abomination@protonmail.com>
2022-04-02 23:10:31 +0200

    Creating an Express app

--- index.mjs
+++ index.mjs
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
