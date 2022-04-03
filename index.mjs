b71d36a87d88d379187b71dff593e302cc816455
saintabomination <saint.abomination@protonmail.com>
2022-04-03 11:54:23 +0200

    Adding presentation action handlers

--- index.mjs
+++ index.mjs
@@ -6,12 +6,28 @@ import { Server } from 'socket.io';
 const app = express();
 const port = 3001;
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
 
 app.use(cors());
 
 io.on('connection', socket => {
-  console.log(`${socket}: Join`);
+  console.log(`${socket.id}: Join`);
+
+  socket.on('move_slide', data => {
+    console.log(`${socket.id}: Move Slide (${data})`);
+  });
+
+  socket.on('reset_presentation', () => {
+    console.log(`${socket.id}: Reset Presentation`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.id}: Disconnect`);
+  });
 });
 
 httpServer.listen(port, () => {
